a9f6589221aedb45a53863d042a5e8d890845d0d
Michael Lin <unadlib@gmail.com>
2017-09-19 11:19:35 +0800

    WIP

--- src/Injector.js
+++ src/Injector.js
@@ -1,17 +1,19 @@
 import is from "../lib/isType"
-
+import sleep from "../lib/sleep"
 export default class Injector {
   constructor(config = {}) {
+    // console.log(Object.keys(this),0)
     const { setParams = this._setParams } = config
     if (is.Function(setParams)) {
       ;this::setParams(config)
     } else {
       throw new Error(`\'setParams\' must be a function.`)
     }
-    this._complete = config.complete
+    this._done = config.complete || status
     this._modules = new Map()
-    this.initialize()
-    this._initiate()
+    this._queueModules = new Set()
+    // this.initialize()
+    // this._initiate()
     this._bootstrap().then(this::this._complete)
   }
 
@@ -23,8 +25,9 @@ export default class Injector {
       })
   }
 
-  _complete() {
+  _complete(callStatus) {
     this.initiated = true
+    callStatus(this.initiated)
     return this
   }
 
@@ -42,17 +45,16 @@ export default class Injector {
     return dependenceMap
   }
 
-  _queue(modules = [], list = []) {
+  _queue(modules = [], list) {
     let restModules = []
-    console.log(modules, 99999)
     modules.map(({ moduleName, dependence = [] } = {}) => {
       const isNoneDependence = dependence.length === 0
-      const rest = [...new Set(dependence.concat(list))].filter(
-        item => !list.includes(item),
+      const rest = [...new Set(dependence.concat([...list]))].filter(
+        item => ![...list].includes(item),
       )
       const isDepended = rest.length === 0
       if (isNoneDependence || isDepended) {
-        list.push(moduleName)
+        list.add(moduleName)
       } else {
         restModules.push({ moduleName, dependence })
       }
@@ -72,12 +74,13 @@ export default class Injector {
   //     .map(({module}) => module)
   // }
 
-  _initiate() {
-    let initializeList = []
+  _initialize() {
     const dependenceMap = this._merge(this._modules)
-    this._queue(dependenceMap, initializeList)
-    initializeList.forEach(moduleName => {
+    this._queue(dependenceMap, this._queueModules)
+    // console.log(this._modules,dependenceMap,initializeList)
+    this._queueModules.forEach(moduleName => {
       const { Module, parameters } = this._modules.get(moduleName)
+
       this[moduleName] = new Module(parameters)
     })
     ;this::this.distribute(dependenceMap)
@@ -102,24 +105,63 @@ export default class Injector {
   }
 
   async _bootstrap() {
-    this._modules.forEach(({}) => {})
+    await sleep()
+    let queueModules = [...this._queueModules]
+    while (queueModules.length > 0) {
+      const moduleName = queueModules.shift()
+      const current = this._modules.get(moduleName)
+      const isAsync = is.AsyncFunction(this[moduleName].initialize)
+      const injectors = [...current.injectors]
+      let beforeInjectors = injectors.filter(({ before }) => before)
+      while (beforeInjectors.length > 0) {
+        let unprocessed
+        const { before, deps } = beforeInjectors.shift()
+        const isAsyncAction = is.AsyncFunction(before)
+        const args = deps.map(dep => this[dep])
+        if (isAsyncAction) {
+          unprocessed = await before(...args, this[moduleName])
+        } else {
+          unprocessed = before(...args, this[moduleName])
+        }
+      }
+      if (isAsync) {
+        await this[moduleName].initialize()
+      } else {
+        this[moduleName].initialize()
+      }
+      let afterInjectors = injectors.filter(({ after }) => after)
+      while (afterInjectors.length > 0) {
+        let processed
+        const { after, deps } = afterInjectors.shift()
+        const isAsyncAction = is.AsyncFunction(after)
+        const args = deps.map(dep => this[dep])
+        if (isAsyncAction) {
+          processed = await after(...args, this[moduleName])
+        } else {
+          processed = after(...args, this[moduleName])
+        }
+      }
+    }
   }
 
-  initialize() {
-    throw new Error(
-      `Please set initialization Dependency Injection configuration.`,
-    )
-  }
+  // initialize() {
+  //   throw new Error(
+  //     `Please set \'initialize\' function for Dependency Injection configuration.`,
+  //   )
+  // }
 
-  inject({ module, deps = [], params = {}, before, after } = {}) {
-    const { name, _injectors = [] } = module
-    const { parameters = params, injectors = _injectors } =
-      this._modules.get(name) || {}
-    this._modules.set(name, {
-      Module: module,
-      parameters: Object.assign(parameters, params),
-      injectors: [...injectors, { deps, before, after }],
+  inject(models) {
+    models.map(({ module, deps = [], params = {}, before, after } = {}) => {
+      const { name, _injectors = [] } = module
+      const { parameters = params, injectors = _injectors } =
+        this._modules.get(name) || {}
+      this._modules.set(name, {
+        Module: module,
+        parameters: Object.assign(parameters, params),
+        injectors: [...injectors, { deps, before, after }],
+      })
     })
+    this._initialize()
     // const initialize = async (...injected) => {
     //   const isAsyncModuleCallback =
     //     is.AsyncFunction(module._callback) || is.Promise(module._callback)
@@ -185,3 +227,87 @@ class Module {
       })
   }
 }
+
+class A extends Module {
+  initialize() {
+    this._a = 1
+    console.log("A")
+  }
+}
+class B extends Module {
+  initialize() {
+    this._b = 1
+    console.log("B")
+  }
+}
+class C extends Module {
+  initialize() {
+    this._b = 1
+    console.log("C")
+  }
+}
+
+class D extends C {
+  async initialize() {
+    await sleep(1000)
+    this._ds = 1
+    console.log("D")
+  }
+}
+
+class Phone extends Injector {
+  constructor(...args) {
+    super(...args)
+    this.inject([
+      {
+        module: C,
+        deps: ["A", "B"],
+        params: {
+          testB: 2,
+        },
+        after: (...args) => {
+          console.log(args)
+          return {}
+        },
+        before: (...args) => {
+          return {}
+        },
+      },
+      {
+        module: A,
+        params: {
+          testA: 2,
+        },
+      },
+      {
+        module: B,
+        deps: ["A"],
+        params: {
+          testB: 2,
+        },
+      },
+    ])
+    // console.log(Object.keys(this._modules),1)
+  }
+}
+
+class Bee extends Phone {
+  constructor(...args) {
+    super(...args)
+    this.inject([
+      {
+        module: D,
+        params: {
+          testB: 1,
+        },
+      },
+    ])
+  }
+}
+
+const a = new Bee()
+// console.log(a)
+//
+// setTimeout(()=>{
+//   console.log(a)
+// },2000)
